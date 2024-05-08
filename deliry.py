dfaadd5f43230fd17c0400abac10626aabf57e23
marcprado <marc.prado@duocuc.cl>
2024-05-07 22:12:57 -0400

    Add files via upload

---
+++ deliry.py
@@ -0,0 +1,31 @@
+import os
+import time
+X=input("Volver al menu")
+pikachu_roll="$4500"
+otaku_roll="$5000"
+pulpo_roll="$5200"
+anguila_roll="$4800"
+
+cod_desc="10%descuento"
+valor=0
+total_productos=(""+pikachu_roll+"+"+otaku_roll+"+"+pulpo_roll+"+"+anguila_roll+" ")
+if print ("¿Posees un codigo de descuento?"):
+si=input("soyotaku")
+elif print ("Codigo no valido"):
+("Reingresar el codigo o volver al menu tecleando la letra "+X+" ")
+
+
+print ("delivery de sushi elije 4 tipos de sushi")
+print ("1. Pikachu de Roll $4500 ")
+print ("2. Otaku Rolll $5000 ")
+print ("3. Pulpo venenoso Roll $5200 ")
+print ("4. Anguila Electrica Roll $4800")
+print ("Seleccione una opcion del 1 a 4 ")
+print ("Subtotal por pagar, "+valor+" ")
+print ("Codigo de descuento, "+cod_desc+" ")
+print ("TOTAL "+total_productos+" ")
+print ("Desea agregar algo mas "1(si)"2(no)"")
+
+
+
+
