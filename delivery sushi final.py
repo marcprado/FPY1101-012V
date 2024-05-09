3579ee830e7d2bf759a177d803f653453675cba1
marcprado <marc.prado@duocuc.cl>
2024-05-08 21:50:51 -0400

    Add files via upload

---
+++ delivery sushi final.py
@@ -0,0 +1,58 @@
+import os
+import time
+time.sleep 2
+
+X=input("Volver al menu")
+si=1
+no=2
+total=0
+pikachu_roll=4500
+otaku_roll=5000
+pulpo_roll=5200
+anguila_roll=4800
+tipo_sushi=input("¿Que tipo de pedido quiere?")
+cod_descuento=input(int("10%de descuento"))
+print ("Codigo no valido, para voler al menu oprime la tecla, x")
+otro_pedido=input("¿Desea agregar algo mas?")
+print("")
+
+
+
+
+print ("delivery de sushi elije 4 tipos de sushi")
+print ("1. Pikachu de Roll $4500 ")
+print ("2. Otaku Rolll $5000 ")
+print ("3. Pulpo venenoso Roll $5200 ")
+print ("4. Anguila Electrica Roll $4800")
+print ("Seleccione una opcion del 1 a 4 ")
+
+while True
+if pikachu_roll == 1
+print("")
+elif:
+
+cod_desc="10%descuento"
+valor=0
+total_productos=input("Valor"+pikachu_roll+"+"+otaku_roll+"+"+pulpo_roll+"+"+anguila_roll+" ")
+
+
+
+
+print("")
+
+print("********************************")
+print("TOTAL PRODUCTOS "+total+ "")
+print("********************************")
+print("Pikachu roll: "+pikachu_roll+"")
+print("Otaku roll: "+otaku_roll+"")
+print("Pulpo venenoso Roll: "+pulpo_roll+"")
+print("Anguila electrica Roll: "+anguila_roll+"")
+print("********************************")
+print ("Subtotal por pagar, "+valor+" ")
+print ("Codigo de descuento, "+cod_desc+" ")
+print ("TOTAL "+total_productos+" ")
+
+
+
+
+
