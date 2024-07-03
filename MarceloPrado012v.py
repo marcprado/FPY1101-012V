70780de06defb1380aecd79f790c1664b99c574d
marcprado <marc.prado@duocuc.cl>
2024-07-02 20:58:09 -0400

    Add files via upload

---
+++ MarceloPrado012v.py
@@ -0,0 +1,47 @@
+
+import time
+import random
+import os
+
+nro_ped = 0
+cliente = ""
+comuna = ""
+saco_5kg = 0
+saco_10kg = 0
+saco_20kg = 0
+total = 0
+san_bernardo= ""
+calerade_tango = ""
+buin = ""
+ruta = san_bernardo, calerade_tango, buin
+
+
+print("Bienvenido a la empresa de reparto de comida para gatos a domicilio CatPremiun ")
+while True == 0:
+        print ("1. Registrar pedido")
+        print ("2. Listar todos los pedidos")
+        print ("3. Imprimir hoja de ruta")
+        print ("4. Salir del programa")
+import random 
+if (1):  
+    print("Seleccione un tipo de saco de comida")
+    input(f":",saco_5kg = "saco de 5 kilos" , saco_10kg = "saco de 10 kilos", saco_20kg = "saco de 20 kilos ")
+    pedido=int(input)
+import os
+if (2):
+    print("Usted selecciono este pedido")
+    input(f": "+pedido+" ")
+import os
+if(3):
+    print("Seleccione alguno de los sectores")
+    input(f": "+ruta+" ")
+elif(4):
+    print("Seleccionaste salir del programa")
+    input("")
+
+
+
+
+
+
+
